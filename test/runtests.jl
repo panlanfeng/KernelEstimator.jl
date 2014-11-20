e7d01739899267fd095e841b7a8a956d68faf2d8
Lanfeng Pan <panlanfeng@gmail.com>
2014-11-20 15:39:01 -0600

    regenerate travis.yml; update the test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@ using KernelEstimator
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+include("testreg.jl")
--- test/testreg.jl
+++ test/testreg.jl
@@ -1,53 +1,58 @@
-# #Multivariate kde and regression
-# x=rand(100,4)
-# y=x * ones(4) + x .^ 2 * ones(4)
-# xeval=rand(50,4)
-# den=kde(xeval, x)
-
-# yfit0=LP0(xeval,x,y)
-
-#confidence band
-# cb=BootstrapCB(100, xeval, x, y)
-
-
-#Goodness of fit
-# function SemiPredict(xdata::Matrix{Float64}, ydata::Vector{Float64})
-#    xb=xdata*Sind(xdata,ydata,GaussianKernel)
-#    LP0(xb,xb,ydata,GaussianKernel,1.0)
-# end
-# BootstrapGoodness(10, x, y, SemiPredict)
-
-
 
 ##Univariate kde and regression
 using Distributions
-x=rand(Normal(), 500)
+x=rand(Normal(10), 500)
 xeval=linspace(minimum(x), maximum(x), 100)
+h = bwlscv(x, gaussiankernel)
+@test h>0
 den=kerneldensity(x, xeval=xeval)
+@test all(den .>= 0)
 
+y=2 .* x.^2 
+b0, b1 = linreg(x, y)
+regfit = b0 .+ b1 .* x
+yfit0=lp0(x, y, xeval=x)
+@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
 
-y=2 .* x.^2 + rand(Normal(), 500)
-yfit0=lp0(x, y, xeval=xeval)
-yfit1=lp1(x, y, xeval=xeval)
-yfit0=npr(x, y, xeval=xeval, reg=lp0)
-yfit1=npr(x, y, xeval=xeval, reg=lp1)
+yfit1=lp1(x, y, xeval=x)
+@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+
+yfit0=npr(x, y, xeval=x, reg=lp0)
+@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+
+yfit1=npr(x, y, xeval=x, reg=lp1)
+@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+
 #confidence band
+yfit1=npr(x, y, xeval=xeval, reg=lp1)
 cb=bootstrapCB(x, y, xeval=xeval)
+#@test mean(cb[2,:] .<= yfit1 .<= cb[1,:]) > .9
+
+
 
 
 ###Bounded kernel density
 
 x = rand(Beta(4,2), 500) * 10
 xeval = linspace(0, 10, 100)
-den=kerneldensity(x, xeval=xeval, kernel=betakernel, lb=0.0,ub=10.0)
-#plot(layer(x=xeval, y=pdf(Beta(4,2), xeval/10), Geom.line), layer(x=xeval, y=den, Geom.line, Theme(default_color=color("black"))))
+h = bwlscv(x./10, betakernel)
+@test h>0
+den=kerneldensity(x, xeval=xeval, kernel=betakernel,h=h, lb=0.0,ub=10.0)
+@test all(den .> 0)
+
 
 ##Bounded local regression
 y=2 .* x.^2 + x.*rand(Normal(0, 5), 500)
-yfit0=npr(x, y, xeval=xeval, reg=lp0, kernel=betakernel,lb=0.0, ub=10.0)
+b0, b1 = linreg(x, y)
+regfit = b0 .+ b1 .* x
+yfit0=npr(x, y, xeval=x, reg=lp0, kernel=betakernel,lb=0.0, ub=10.0)
+@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+
+yfit1=npr(x, y, xeval=x, reg=lp1, kernel=betakernel, lb=0.0,ub=10.0)
+@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+
 yfit1=npr(x, y, xeval=xeval, reg=lp1, kernel=betakernel, lb=0.0,ub=10.0)
 cb=bootstrapCB(x, y, xeval=xeval,reg=lp1, kernel=betakernel, lb=0.0,ub=10.0)
-#plot(layer(x=x, y=y, Geom.point), layer(x=xeval, y=yfit1, Geom.line, Theme(default_color=color("black"))))
+#@test mean(cb[2,:] .<= yfit1 .<= cb[1,:]) > .9
 
-# using Gadfly
-# plot(layer(x=x, y=y, Geom.point), layer(x=xeval, y=yfit1, Geom.line, Theme(default_color=color("black"))), layer(x=xeval, y=cb[1,:], Geom.line, Theme(default_color=color("red"))), layer(x=xeval, y=cb[2,:], Geom.line, Theme(default_color=color("red"))))
+
