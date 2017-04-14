a3f3472d7af55b5873071e2ba7401370912caee1
Lanfeng Pan <panlanfeng@gmail.com>
2017-04-13 22:09:32 -0500

    deprecation in v0.6

--- test/testreg.jl
+++ test/testreg.jl
@@ -12,16 +12,16 @@ y=2 .* x.^2 .+ rand(Normal(0,5),500)
 b0, b1 = linreg(x, y)
 regfit = b0 .+ b1 .* x
 yfit0=localconstant(x, y, xeval=x)
-@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit0.-y)
 
 yfit1=locallinear(x, y, xeval=x)
-@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit1.-y)
 
 yfit0=npr(x, y, xeval=x, reg=localconstant)
-@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit0.-y)
 
 yfit1=npr(x, y, xeval=x, reg=locallinear)
-@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit1.-y)
 
 #confidence band
 yfit1=npr(x, y, xeval=xeval, reg=locallinear)
@@ -43,10 +43,10 @@ y=2 .* x.^2 + x.*rand(Normal(0, 5), 500)
 b0, b1 = linreg(x, y)
 regfit = b0 .+ b1 .* x
 yfit0=npr(x, y, xeval=x, reg=localconstant, kernel=gammakernel,lb=0.0)
-@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit0.-y)
 
 yfit1=npr(x, y, xeval=x, reg=locallinear, kernel=gammakernel, lb=0.0)
-@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit1.-y)
 
 #bounded beta kernel density and regression
 x = rand(Beta(4,2), 500) * 10
@@ -62,10 +62,10 @@ y=2 .* x.^2 + x.*rand(Normal(0, 5), 500)
 b0, b1 = linreg(x, y)
 regfit = b0 .+ b1 .* x
 yfit0=npr(x, y, xeval=x, reg=localconstant, kernel=betakernel,lb=0.0, ub=10.0)
-@test sumabs2(regfit .- y) > sumabs2(yfit0.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit0.-y)
 
 yfit1=npr(x, y, xeval=x, reg=locallinear, kernel=betakernel, lb=0.0,ub=10.0)
-@test sumabs2(regfit .- y) > sumabs2(yfit1.-y)
+@test sum(abs2, regfit .- y) > sum(abs2, yfit1.-y)
 
 yfit1=npr(x, y, xeval=xeval, reg=locallinear, kernel=betakernel, lb=0.0,ub=10.0)
 cb=bootstrapCB(x, y, xeval=xeval,reg=locallinear, kernel=betakernel, lb=0.0,ub=10.0)
